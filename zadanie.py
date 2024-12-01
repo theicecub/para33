01a5f33bf69269ecd59b982cad5e7f0d7e4fa7a8
Калбиатулы Абылайхан <@lcoast.itstep>
2024-12-01 11:42:27 +0500

    last task

--- zadanie.py
+++ zadanie.py
@@ -7,3 +7,17 @@ except (ZeroDivisionError, ValueError):
     print("Деление на ноль или неправильная конвертация типов")
 else:
     print(c)
+
+#2
+try:
+    isEven=False
+    num=int(input())
+    if(num % 2 == 0):
+        isEven = True
+except ValueError:
+    print("Неправильная конвертация типов")
+else:
+    if(isEven == True):
+        print("Число четное")
+    else:
+        print("Число нечетное")
